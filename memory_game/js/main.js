c74f8d59db0ae77dc061c6e3cef5195b58fdaa91
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-08-17 19:58:10 +0800

    Implement base game logic via DOM

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -17,6 +17,16 @@ var cards = [{
 }];
 var cardsInPlay = [];
 
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
 var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -27,13 +37,14 @@ var checkForMatch = function() {
   };
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id')
+  this.setAttribute('src', cards[cardId].cardImage)
   console.log('User Flipped ' + cards[cardId].rank);
   console.log(cards[cardId].suit);
   console.log(cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
+  checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+createBoard();
